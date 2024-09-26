b95b4aa543e694e647c69d20147473249c365664
jhkim105 <jhkim105@gmail.com>
2024-09-26 15:27:50 +0900

    spring AccountRepositoryTest

---
+++ spring/src/test/kotlin/jhkim105/tutorials/spring/repository/AccountRepositoryTest.kt
@@ -0,0 +1,25 @@
+package jhkim105.tutorials.spring.repository
+
+import org.assertj.core.api.Assertions.assertThat
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.test.context.jdbc.Sql
+import java.math.BigDecimal
+import kotlin.test.Test
+
+
+@DataJpaTest
+class AccountRepositoryTest {
+
+    @Autowired
+    lateinit var accountRepository: AccountRepository
+
+
+    @Test
+    @Sql("AccountRepositoryTest.sql")
+    fun findById() {
+        assertThat(accountRepository.findByIdOrNull("tid01")?.balance).isEqualTo(BigDecimal("100.00"))
+    }
+
+}
